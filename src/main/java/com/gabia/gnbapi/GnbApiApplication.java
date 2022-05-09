a653837cb8fd358d09b867f4e9d2567512fed9be
Groot(김명석) <mskim@gabia.com>
2022-05-10 08:08:18 +0900

    init project

---
+++ src/main/java/com/gabia/gnbapi/GnbApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabia.gnbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnbApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GnbApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabia/gnbapi/GnbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabia.gnbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnbApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
